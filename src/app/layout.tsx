6d0c7b9c5e4a5a224a00704f7bac6d18c3edc240
adarsh <163159242+axdrsh@users.noreply.github.com>
2025-01-30 13:58:32 +0530

    abcd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/mode-toggle";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -19,19 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
-          disableTransitionOnChange
-        >
-          <div className="absolute right-4 top-4">
-            <ModeToggle />
-          </div>
-          <main className="min-h-screen p-8 md:p-24 max-w-2xl mx-auto">
-            {children}
-          </main>
-        </ThemeProvider>
+        <main className="min-h-screen p-8 md:p-24 max-w-2xl mx-auto">
+          {children}
+        </main>
       </body>
     </html>
   );
