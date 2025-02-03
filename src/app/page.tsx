d66dd3c939d847aee33cf2b4a84b34f343747618
adarsh <163159242+axdrsh@users.noreply.github.com>
2025-02-03 11:30:43 +0530

    revamped the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,36 @@
+import Image from "next/image";
+import { ExternalLink } from "lucide-react";
+
 export default function Page() {
   return (
-    <main className="max-w-2xl mx-auto px-4 py-12 space-y-4">
-      <h1 className="text-2xl font-normal">adarsh.</h1>
+    <main className="max-w-xl mx-auto px-4 py-12 space-y-4 font-mono">
+      <Image
+        src="/lucy.jpeg"
+        alt="Profile picture"
+        width={64}
+        height={64}
+        className="rounded-sm"
+        priority
+      />
+
+      <h1 className="text-xl">adarsh.</h1>
 
       <p>
-        1st year college student. likes to learn and build stuff for fun. i
-        build softwares that are actually useful and my study interests mainly
-        revolve around ai and machine learning. i also have a passion for cinema
-        and music.
-      </p>
-
-      <p>
-        checkout my projects on{" "}
-        <a className="underline" href="https://github.com/axdrsh">
-          github
-        </a>
-        .
-      </p>
-
-      <p>
-        if you wanna talk, DM me on{" "}
-        <a className="underline" href="https://x.com/axdrsh">
-          X
+        hi, i'm a 1st year college student. i like building useful software and
+        my interests mainly revolve around ai/machine learning and open source.
+        you can find me on{" "}
+        <a
+          href="https://github.com/axdrsh"
+          className="underline hover:text-gray-600 inline-flex items-center"
+        >
+          github <ExternalLink className="w-3 h-3 ml-0.5" />
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://x.com/axdrsh"
+          className="underline hover:text-gray-600 inline-flex items-center"
+        >
+          x <ExternalLink className="w-3 h-3 ml-0.5" />
         </a>
         .
       </p>
