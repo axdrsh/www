f45891e84a252f48283cea5653c41bb654f22f82
adarsh <163159242+axdrsh@users.noreply.github.com>
2025-08-22 01:40:25 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Page() {
         <ul className="space-y-1 text-sm">
           <li>deep in backend dev</li>
           <li>building useless and sometimes useful projects</li>
-          <li>dabbling into ml and systems</li>
+          <li>dabbling into ml/dl and systems</li>
         </ul>
       </div>
 
